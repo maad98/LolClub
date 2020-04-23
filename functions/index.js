885edf03f90cf265bf85d87bf5930701dc1d5984
Maad Abduljaleel <maad1998@hotmail.co.uk>
2020-04-23 03:32:25 -0400

    v1
    first update

---
+++ functions/index.js
@@ -0,0 +1,62 @@
+const functions = require('firebase-functions');
+
+const { Kayn, REGIONS } = require('kayn');
+const apiKey = 'RGAPI-9197a94a-5a17-422a-aafb-1dba9bd50b72';
+
+const kayn = Kayn(apiKey)({
+    region: REGIONS.NORTH_AMERICA,
+    apiURLPrefix: 'https://%s.api.riotgames.com',
+    locale: 'en_US',
+    debugOptions: {
+        isEnabled: true,
+        showKey: false,
+    },
+    requestOptions: {
+        shouldRetry: true,
+        numberOfRetriesBeforeAbort: 3,
+        delayBeforeRetry: 1000,
+        burst: false,
+        shouldExitOn403: false,
+    },
+    cacheOptions: {
+        cache: null,
+        timeToLives: {
+            useDefault: false,
+            byGroup: {},
+            byMethod: {},
+        },
+    },
+})
+
+
+exports.getHistory = functions.https.onRequest(async(request, response) => {
+        const { accountId } = await kayn.Summoner.by.name('Eyta')
+        const { matches } = await kayn.Matchlist.by
+            .accountID(accountId)
+            .query({ queue: 420 })
+        const gameIds = matches.slice(0, 10).map(({ gameId }) => gameId)
+        const requests = gameIds.map(kayn.Match.get)
+        const results = await Promise.all(requests)
+        var wantedData = [];
+        results.forEach(result=> {
+            let x = result.participantIdentities.participantId;
+            x.forEach(particId => {
+                wantedData.push(particId);
+                wantedData.push(result.participantIdentities.player.summonerName);
+                wantedData.push(result.teams.win);
+                wantedData.push(result.participants.spell1Id);
+                wantedData.push(result.participants.spell2Id);
+                wantedData.push(result.participants.kills);
+                wantedData.push(result.participants.deaths);
+                wantedData.push(result.participants.assists);
+                wantedData.push(result.participants.championId);
+                wantedData.push('-------------------------------------------');
+            })
+           
+       })
+        console.log(results)
+        response.send(results);
+        
+
+    });
+    
