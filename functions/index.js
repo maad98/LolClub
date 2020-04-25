ce0cf47e3e82000b9a05e76d22e88cd001ad8a50
Yusuf Fathi <youeesfathi@gmail.com>
2020-04-26 00:47:38 +0200

    v13

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 
 const { Kayn, REGIONS } = require('kayn');
-const apiKey = 'RGAPI-c0810f8f-9ad1-4545-bc6c-2e78cccd40c0';
+const apiKey = 'RGAPI-8cbffa00-207a-45a1-b6ca-589f8e704109';
 
 const kayn = Kayn(apiKey)({
     region: REGIONS.NORTH_AMERICA,
@@ -37,7 +37,7 @@ exports.getHistory = functions.https.onRequest(async(request, response) => {
         const { accountId } = await kayn.Summoner.by.name(givenSummName)
         const { matches } = await kayn.Matchlist.by
             .accountID(accountId)
-            .query({ queue: 420  })
+            .query({ queue: [420, 440]  })
         const gameIds = matches.slice(0, 10).map(({ gameId }) => gameId)
         const requests = gameIds.map(kayn.Match.get)
         const results = await Promise.all(requests)
@@ -51,7 +51,8 @@ exports.getHistory = functions.https.onRequest(async(request, response) => {
                     let matchWantedData={
                         name : result.participantIdentities[i].player.summonerName,
                         win : (result.participants[i].participantId > 5) ? result.teams[1].win : result.teams[0].win,
-                        data : result.gameCreation,
+                        date : result.gameCreation,
+                        queue : result.queueId || 'none',
                         duration: result.gameDuration,
                         champion: result.participants[i].championId,
                         spell1 : result.participants[i].spell1Id,
@@ -59,6 +60,9 @@ exports.getHistory = functions.https.onRequest(async(request, response) => {
                         kills : result.participants[i].stats.kills,
                         deaths : result.participants[i].stats.deaths,
                         assists : result.participants[i].stats.assists,
+                        //KP
+                        //Gold
+                        //CS
                     };
                     wantedData.push(matchWantedData);
                     break;
@@ -70,41 +74,4 @@ exports.getHistory = functions.https.onRequest(async(request, response) => {
         response.send(wantedData);
     });
     
-
-
-
-    // if(result.participants[i].participantId > 5)
-    // {
-    //     //wantedData[cntr][0]=result.teams[1].win;
-    // }
-    // else
-    // {
-    //     //wantedData[cntr][0]=result.teams[0].win;
-    // }
-
-    //   wantedData[cntr][0].push(result.gameCreation);
-    //   wantedData[cntr][1].push(result.gameDuration);
-    //   wantedData[cntr][2].push(result.participantIdentities[i].player.summonerName);
-    //   wantedData[cntr][3].push(result.participants[i].spell1Id);
-    //   wantedData[cntr][4].push(result.participants[i].spell2Id);
-    //   wantedData[cntr][5].push(result.participants[i].stats.kills);
-    //   wantedData[cntr][6].push(result.participants[i].stats.deaths);
-    //   wantedData[cntr][7].push(result.participants[i].stats.assists);
-    //   wantedData[cntr][8].push(result.participants[i].championId);
-    //   wantedData[cntr][9].push('--------------------------------------------------------');
-
-
-
-
-    // wantedData[cntr][0]= (result.participants[i].participantId > 5) ? wantedData[cntr][0]=result.teams[1].win : wantedData[cntr][0]=result.teams[0].win;
-    //                 wantedData[cntr][1]=result.gameCreation;
-    //                 wantedData[cntr][2]=result.gameDuration;
-    //                 wantedData[cntr][3]=result.participantIdentities[i].player.summonerName;
-    //                 wantedData[cntr][4]=result.participants[i].spell1Id;
-    //                 wantedData[cntr][5]=result.participants[i].spell2Id;
-    //                 wantedData[cntr][6]=result.participants[i].stats.kills;
-    //                 wantedData[cntr][7]=result.participants[i].stats.deaths;
-    //                 wantedData[cntr][8]=result.participants[i].stats.assists;
-    //                 wantedData[cntr][9]=result.participants[i].championId;
-    //                 console.log('cntr is '+cntr+'   '+wantedData[cntr]);
-    //                 break;
+   
