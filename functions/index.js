842db82ec106c7f83350dc35be5ec0c18280ff0a
Yusuf Fathi <youeesfathi@gmail.com>
2020-05-04 20:54:38 +0200

    getHistory

--- functions/index.js
+++ functions/index.js
@@ -4,13 +4,14 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require("./lolclub-firebase-adminsdk-px2rx-e43ef1eb4a.json");
 
+const { Kayn, REGIONS } = require('kayn');
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://lolclub.firebaseio.com"
 });
 
-const { Kayn, REGIONS } = require('kayn');
-const apiKey = 'RGAPI-06b726dc-b966-43f7-bbd8-937247f2da16';
+const apiKey = 'RGAPI-c46ca641-ece1-44bb-bc8d-8c053af3aa42';
 
 let kayn = Kayn(apiKey)({
     region: REGIONS.EUROPE_WEST,
@@ -24,7 +25,7 @@ let kayn = Kayn(apiKey)({
         shouldRetry: true,
         numberOfRetriesBeforeAbort: 3,
         delayBeforeRetry: 1000,
-        burst: true,
+        burst: false,
         shouldExitOn403: false,
     },
     cacheOptions: {
@@ -38,180 +39,123 @@ let kayn = Kayn(apiKey)({
 })
 
 exports.getHistory = functions.https.onRequest(async(request, response) => {
-        let givenSummName = request.query.name || 'Last WarriorX';
-        let givenServer=request.query.server || 'euw';
-        let givenRegion=getRegion(givenServer);
+        //let givenSummName = request.query.name || 'Last WarriorX';
+        //let givenServer=request.query.server || 'euw';
+        let givenID=request.query.id || '7STkMZfaPh';
         
-        console.log(givenRegion);
         try{   
-        const { accountId } = await kayn.Summoner.by.name(givenSummName)
-        .region(givenServer)
-        const { matches } = await kayn.Matchlist.by
-            .accountID(accountId)
+            let userData =await getUserInfoByID(givenID);
+            let givenSummName=userData.ign;
+            let givenServer=userData.server;
+            console.log(givenSummName+ " / "+ givenServer);
+            let givenRegion=getRegion(givenServer);
+            let userRank=await getRank(givenSummName,givenServer);
+            const { accountId } = await kayn.Summoner.by.name(givenSummName)
             .region(givenServer)
-            .query({ queue: [420, 440]  }) 
-        const gameIds = matches.slice(0, 10).map(({ gameId }) => gameId)
-        const requests = gameIds.map(kayn.Match.get).map(x => x.region(givenRegion));
-        const results = await Promise.all(requests)
-        var wantedData = [];
-        results.forEach(result=> {
-            for(i = 0; i < 10; i++)
-            {
-                if(result.participantIdentities[i].player.summonerName === givenSummName.toString())
+            const { matches } = await kayn.Matchlist.by
+                .accountID(accountId)
+                .region(givenServer)
+                .query({ queue: [420, 440]  }) 
+            const gameIds = matches.slice(0, 10).map(({ gameId }) => gameId)
+            const requests = gameIds.map(kayn.Match.get).map(x => x.region(givenRegion));
+            const results = await Promise.all(requests)
+            var wantedData = [];
+            let userInfo={
+                    rank : userRank,
+                    mainLane : userData.mainLane,
+                    secondaryLane : userData.secondaryLane,
+                    gender : userData.gender,
+            };
+            wantedData.push(userInfo);
+            results.forEach(result=> {
+                for(i = 0; i < 10; i++)
                 {
-                    let totalMinions = result.participants[i].stats.totalMinionsKilled +
-                    result.participants[i].stats.neutralMinionsKilled;
+                    if(result.participantIdentities[i].player.summonerName === givenSummName.toString())
+                    {
+                        let totalMinions = result.participants[i].stats.totalMinionsKilled +
+                        result.participants[i].stats.neutralMinionsKilled;
 
-                    let ka = result.participants[i].stats.kills +result.participants[i].stats.assists
+                        let ka = result.participants[i].stats.kills +result.participants[i].stats.assists
 
-                    let matchWantedData={
-                        name : result.participantIdentities[i].player.summonerName,
-                        win : (result.participants[i].participantId > 5) ? result.teams[1].win : result.teams[0].win,
-                        date : result.gameCreation,
-                        queue : result.queueId ,
-                        duration: result.gameDuration,
-                        champion: result.participants[i].championId,
-                        spell1 : result.participants[i].spell1Id,
-                        spell2 : result.participants[i].spell2Id,
-                        kills : result.participants[i].stats.kills,
-                        deaths : result.participants[i].stats.deaths,
-                        assists : result.participants[i].stats.assists,
-                        minions: totalMinions,
-                        vision: result.participants[i].stats.visionScore,
-                        kp: (result.participants[i].participantId > 5) ? Math.round(ka/getKp(result, true) * 100) : Math.round(ka/getKp(result, false)* 100)
-                    };
-                    wantedData.push(matchWantedData);
-                    break;
+                        let matchWantedData={
+                            name : result.participantIdentities[i].player.summonerName,
+                            win : (result.participants[i].participantId > 5) ? result.teams[1].win : result.teams[0].win,
+                            date : result.gameCreation,
+                            queue : result.queueId ,
+                            duration: result.gameDuration,
+                            champion: result.participants[i].championId,
+                            spell1 : result.participants[i].spell1Id,
+                            spell2 : result.participants[i].spell2Id,
+                            kills : result.participants[i].stats.kills,
+                            deaths : result.participants[i].stats.deaths,
+                            assists : result.participants[i].stats.assists,
+                            minions: totalMinions,
+                            vision: result.participants[i].stats.visionScore,
+                            kp: (result.participants[i].participantId > 5) ? Math.round(ka/getKp(result, true) * 100) : Math.round(ka/getKp(result, false)* 100)
+                        };
+                        wantedData.push(matchWantedData);
+                        break;
+                    }
                 }
-            }
-            
-       })
+                
+        })
     }catch(e){
         console.log(e);
     }
         response.send(wantedData);
        
-    });
-
-function getKp(result, flag){
-    // var index = (bool == true) ? 5 : 0;
-    let sum = 0;
-    if(flag){
-        for(i = 5; i < 10; i++){
-          sum += result.participants[i].stats.kills;
-        }
-      }
-      else{
-        for(i = 0; i < 5; i++){
-          sum += result.participants[i].stats.kills;
-        }
-      }
-      return sum;
-}
-    
-function getRegion(givenServer){
-    switch(givenServer) {
-        case 'euw':
-            return REGIONS.EUROPE_WEST;
-        case 'na':
-            return REGIONS.NORTH_AMERICA;
-        case 'eune':
-            return REGIONS.EUROPE;
-        case 'oce':
-            return REGIONS.OCEANIA;
-        case 'br':
-            return REGIONS.BRAZIL;
-        case 'las':
-            return REGIONS.LATIN_AMERICA_SOUTH;
-        case 'tur':
-            return REGIONS.TURKEY;
-        case 'lan':
-            return REGIONS.LATIN_AMERICA_NORTH;        
-        case 'ru' :
-            return REGIONS.RUSSIA;         
-        case 'jp' :
-            return REGIONS.JAPAN;         
-        case 'kr':
-            return REGIONS.KOREA; 
-        default: 
-            return REGIONS.NORTH_AMERICA;
-      }
-}
-
-exports.confirmIGNAndCreateUserDocument = functions.https.onRequest(async(request, response) => {
-    let givenSummName = request.query.name || 'LastWarriorX';
-    let givenServer=request.query.server || 'euw';
-    let givenCode=request.query.code || '1234';
-    let id=makeid(10);
-    try{
-        let isVerified=await checkCode(givenSummName,givenServer,givenCode);
-        if(!isVerified) //the provided code by the user is wrong
-        {
-            let responseBody={
-                status:404,
-                message : 'Unsuccessful : Wrong Code',
-            };
-            response.send(responseBody);
-        }
-        else{
-            var db = admin.database();
-            var ref = db.ref("/users/"+id);
-            ref.set({
-                info : {
-                ign :givenSummName,
-                server :givenServer,
-                email: 'none',
-                gender:'none',
-                mainlane:'none',
-                secondarylane:'none',
-                imageurl:'none',
-                isVerified:true,
-                isEmailVerified:false,
-                datecreated: Date.now(),
-                solorank:'none',
-                flexrank:'none',
-                friends:'none',
-                clashteams:'none',
-            }
-            });
-            let responseBody={
-                status:200,
-                message : 'Successful : User Created',
-                id : id,
-            };
-            response.send(responseBody);
-        }
-    }
-    catch(e){
-        console.log(e);
-        response.send(e);
-    }
-    
 });
 
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
- }
-
-async function  checkCode (givenSummName,givenServer,givenCode){
-    
-    const { id: myID } = await kayn.Summoner.by.name(givenSummName).region(givenServer)
-
-    const code = await kayn.ThirdPartyCode.by.summonerID(myID).region(givenServer)
-    
-    console.log("the code is "+code);
-
-    console.log(code);
-
-    return code===givenCode;
-}
-
+exports.confirmIGNAndCreateUserDocument = functions.https.onRequest(async(request, response) => {
+        let givenSummName = request.query.name || '7 Dead Lee Sins';
+        let givenServer=request.query.server || 'euw';
+        let givenCode=request.query.code || '1234';
+        let id=makeid(10);
+        try{
+            let isVerified=await checkCode(givenSummName,givenServer,givenCode);
+            if(!isVerified) //the provided code by the user is wrong
+            {
+                let responseBody={
+                    status:404,
+                    message : 'Unsuccessful : Wrong Code',
+                };
+                response.send(responseBody);
+            }
+            else{
+                var db = admin.database();
+                var ref = db.ref("/users/"+id);
+                ref.set({
+                    info : {
+                    ign :givenSummName,
+                    server :givenServer,
+                    email: 'none',
+                    gender:'none',
+                    mainlane:'none',
+                    secondarylane:'none',
+                    imageurl:'none',
+                    isVerified:true,
+                    isEmailVerified:false,
+                    datecreated: Date.now(),
+                    solorank:'none',
+                    flexrank:'none',
+                    friends:'none',
+                    clashteams:'none',
+                }
+                });
+                let responseBody={
+                    status:200,
+                    message : 'Successful : User Created',
+                    id : id,
+                };
+                response.send(responseBody);
+            }
+        }
+        catch(e){
+            console.log(e);
+            response.send(e);
+        }
+        
+});
 
 exports.updateProfile=functions.https.onRequest(async(request, response) => {
     let givenID = request.query.id;
@@ -224,7 +168,7 @@ exports.updateProfile=functions.https.onRequest(async(request, response) => {
     var flexBox=null;
     var soloBox=null;
 
-    var userData=await getIGNandServerByID(givenID);
+    var userData=await getUserInfoByID(givenID);
     console.log(await userData);
 
     var rankData=await getRank(userData.ign,userData.server);
@@ -280,6 +224,89 @@ exports.updateProfile=functions.https.onRequest(async(request, response) => {
     }
 });
 
+exports.testing=functions.https.onRequest(async(request, response) => {
+    try{
+        let userData=await getUserInfoByID('knBVNytag1');
+        console.log(userData.ign+" / "+userData.server);
+
+        let userRank=await getRank('7 Dead Lee Sins','euw');
+        console.log(userRank);
+
+        response.send(userRank);
+    }
+    catch(e){
+        response.send(e);
+    }
+});
+
+function getKp(result, flag){
+    // var index = (bool == true) ? 5 : 0;
+    let sum = 0;
+    if(flag){
+        for(i = 5; i < 10; i++){
+          sum += result.participants[i].stats.kills;
+        }
+      }
+      else{
+        for(i = 0; i < 5; i++){
+          sum += result.participants[i].stats.kills;
+        }
+      }
+      return sum;
+}
+    
+function getRegion(givenServer){
+    switch(givenServer) {
+        case 'euw':
+            return REGIONS.EUROPE_WEST;
+        case 'na':
+            return REGIONS.NORTH_AMERICA;
+        case 'eune':
+            return REGIONS.EUROPE;
+        case 'oce':
+            return REGIONS.OCEANIA;
+        case 'br':
+            return REGIONS.BRAZIL;
+        case 'las':
+            return REGIONS.LATIN_AMERICA_SOUTH;
+        case 'tur':
+            return REGIONS.TURKEY;
+        case 'lan':
+            return REGIONS.LATIN_AMERICA_NORTH;        
+        case 'ru' :
+            return REGIONS.RUSSIA;         
+        case 'jp' :
+            return REGIONS.JAPAN;         
+        case 'kr':
+            return REGIONS.KOREA; 
+        default: 
+            return REGIONS.NORTH_AMERICA;
+      }
+}
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+
+async function  checkCode (givenSummName,givenServer,givenCode){
+    
+    const { id: myID } = await kayn.Summoner.by.name(givenSummName).region(givenServer)
+
+    const code = await kayn.ThirdPartyCode.by.summonerID(myID).region(givenServer)
+    
+    console.log("the code is "+code);
+
+    console.log(code);
+
+    return code===givenCode;
+}
+
 async function getRank(givenSummName,givenServer){
     try{
         const { id: myID } = await kayn.Summoner.by.name(givenSummName).region(givenServer);
@@ -314,28 +341,16 @@ async function getRank(givenSummName,givenServer){
     }
  }
 
-exports.testing=functions.https.onRequest(async(request, response) => {
-    try{
-        let userData=await getIGNandServerByID('knBVNytag1');
-        console.log(userData.ign+" / "+userData.server);
-
-        let userRank=await getRank('7 Dead Lee Sins','euw');
-        console.log(userRank);
-
-        response.send(userRank);
-    }
-    catch(e){
-        response.send(e);
-    }
-});
-
-async function getIGNandServerByID(id){
+async function getUserInfoByID(id){
     var db = admin.database();
     var ref = db.ref("/users/"+id+"/info/");
     return new Promise((resolve, reject) => {
         ref.on('value', snapshot => resolve({
             ign : snapshot.val().ign,
             server : snapshot.val().server,
+            mainLane : snapshot.val().mainlane,
+            secondaryLane : snapshot.val().secondarylane,
+            gender : snapshot.val().gender
         }), reject)
     })
 }
